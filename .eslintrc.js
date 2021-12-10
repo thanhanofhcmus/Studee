1689268d916938477e6d6573374722849e19d35e
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2021-12-10 21:22:21 +0700

    Change from yarn to npm, add eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021
+  },
+  rules: {
+    semi: [2, 'always'],
+    indent: ['error', 2],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }]
+  }
+};
