233cea035fd611bc1dd0687f38854cca0202e61b
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2021-12-11 16:53:49 +0700

    FIx courses router name, reformat ejs files, remove unused next parameter

--- routes/course.js
+++ routes/course.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 /* GET Course home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('course/courses', { title: 'Các khoá học' });
 });
 
-router.get('/course-details', (req, res, next) => {
+router.get('/course-details', (req, res) => {
   res.render('course/course-details', { title: 'Chi tiết khoá học' });
 });
 
