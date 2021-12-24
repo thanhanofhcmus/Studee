e982180eccb92aac3f77739343d0e6bbc4716f55
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2021-12-24 16:06:15 +0700

    Abstract error middleware

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const flash = require('connect-flash');
+
+const errorMiddleware = require('./middleware/error');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -42,20 +43,7 @@ app.use('/courses', courseRouter);
 app.use('/profile', profileRouter);
 app.use('/auth', authRouter);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorMiddleware.notFound);
+app.use(errorMiddleware.error);
 
 module.exports = app;
