7ca5de2134b5369aa0d1a8c1ea59606eaa5fb9ac
19120600 <19120600@student.hcmus.edu.vn>
2021-12-24 20:41:50 +0700

    Adding profile2

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/teachers', teacherRouter);
 app.use('/courses', courseRouter);
-app.use('/profile', profileRouter);
+app.use('/profiles', profileRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/profile');
 
 router.get('/', controller.list);
-
+router.get('/edit', controller.edit);
+router.post('/update', controller.update);
 module.exports = router;
