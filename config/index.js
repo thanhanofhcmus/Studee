fb0a0ccfb390a9ba4ca8e9d4d4304ba350307b90
Nguyễn Thanh An <thanhan030301@gmail.com>
2021-12-31 16:30:25 +0700

    Chnage config session key

--- config/index.js
+++ config/index.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   AZURE_USERNAME: process.env.AZURE_USERNAME,
   AZURE_PASSWORD: process.env.AZURE_PASSWORD,
-  SECRET_KEY: process.env.SECRET_KEY
+  SESSION_SECRET: process.env.SESSION_SECRET
 };
